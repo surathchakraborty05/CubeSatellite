7d0a987231e54ff037a2f5e83f1739aa0cdcb982
surathchakraborty05 <surathchakraborty12@gmail.com>
2026-05-19 21:13:11 +0530

    added turbopack

--- next.config.mjs
+++ next.config.mjs
@@ -3,24 +3,28 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+
   images: {
     unoptimized: true,
   },
+
+  turbopack: {},
+
   webpack: (config, { isServer }) => {
     if (isServer) {
       config.externals = [
         ...(config.externals || []),
-        '@luma.gl/core',
-        '@luma.gl/webgl',
-        'deck.gl',
-        '@deck.gl/core',
-        '@deck.gl/react',
-        '@deck.gl/aggregation-layers',
+        "@luma.gl/core",
+        "@luma.gl/webgl",
+        "deck.gl",
+        "@deck.gl/core",
+        "@deck.gl/react",
+        "@deck.gl/aggregation-layers",
       ];
     }
+
     return config;
   },
-}
+};
 
-
-export default nextConfig
+export default nextConfig;
